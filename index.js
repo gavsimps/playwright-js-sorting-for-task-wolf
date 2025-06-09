0f9f86f996e5099fe4d1508dbb489ead4cfd94eb
gavsimps <gavsimps@iu.edu>
2025-06-09 11:50:57 -0400

    changed commenting

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
+
 // const { chromium } = require("playwright");
-
-// async function sortHackerNewsArticles() {
-//   // launch browser
-//   const browser = await chromium.launch({ headless: false });
-//   const context = await browser.newContext();
-//   const page = await context.newPage();
-
-//   // go to Hacker News
-//   await page.goto("https://news.ycombinator.com/newest");
-// }
-
-// (async () => {
-//   await sortHackerNewsArticles();
-// })();
 
 // ---------------------------------------
 //          Firefox Browser Tests
@@ -24,6 +11,7 @@ const {firefox} = require("playwright");
 async function sortHackerNewsArticles() {
   // launch browser
   const browser = await firefox.launch({ headless: false });
+  //   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
 
